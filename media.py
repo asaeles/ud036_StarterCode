3e93bf9e5f50168fe68e204215b7b7c1b58687ff
Ahmad Sulaeman <asaeles@gmail.com>
2018-10-16 20:32:25 +0200

    Add Movie class
    Added the library media.py containing one class Movie required for the project

---
+++ media.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+class Movie(object):
+	""" Holds the class varibles required for displaying a movie
+	in Fresh Tomatoes HTML generator and playing its YouTube trailer"""
+
+	def __init__(self, movie_title, movie_year, movie_storyline,
+	movie_poster_url, movie_trailer_url):
+	""" Constructor method, sets the instance variables """
+		self.title = movie_title
+		self.year = movie_year
+		self.storyline = movie_storyline
+		self.poster_image_url = movie_poster_url
+		self.trailer_youtube_url = movie_trailer_url
+
+	def play_youtube_video(self):
+		""" Displays the YouTube trailer associated with
+		the movie within the same web-browser page """
+		webbrowser.open(self.trailer_youtube_url)
