37a3f0c04c6ae23feab7951a9f61b45df1d88dd7
Ahmad Sulaeman <asaeles@gmail.com>
2018-10-19 13:57:08 +0200

    Conform to PEP8 Requirements
    Update the scripts to conform to PEP8 requirements found on http://pep8online.com/checkresult.
    
    Update docstrings to conform PEP requirements found on https://blog.dolphm.com/pep257-good-python-docstrings-by-example/.

--- media.py
+++ media.py
@@ -1,13 +1,23 @@
 class Movie(object):
-	""" Holds the class varibles required for displaying a movie
-	in Fresh Tomatoes HTML generator and playing its YouTube trailer"""
 
-	def __init__(self, movie_title, movie_year, movie_storyline,
-	movie_poster_url, movie_trailer_url):
-		""" Constructor method, sets the instance variables """
-		self.title = movie_title
-		self.year = movie_year
-		self.storyline = movie_storyline
-		self.poster_image_url = movie_poster_url
-		self.trailer_youtube_url = movie_trailer_url
+    """Movie class for use with Fresh Tomatoes.
 
+    This class acts as a container for the parameters required by the
+    Fresh Tomatoes script to display a movie in the HTML movie list.
+
+    """
+
+    def __init__(self, movie_title, movie_year, movie_storyline,
+                 movie_poster_url, movie_trailer_url):
+        """Constructor method, sets the instance variables.
+
+        All the instance variables are mandatory for initialization, the required
+        values are the movie: Title, Release Year, Storyline, Poster URL and
+        YouTube Trailer URL.
+
+        """
+        self.title = movie_title
+        self.year = movie_year
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_url
+        self.trailer_youtube_url = movie_trailer_url
